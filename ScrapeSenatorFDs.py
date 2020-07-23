38edd5046bcadbb30aff28d2bd7490d8ab2f282e
rcdodds <rcdodds@users.noreply.github.com>
2020-07-23 00:35:03 -0400

    Implement SQLite database
    - Switch to logging data in a SQLite database
    - Tidied up code

--- ScrapeSenatorFDs.py
+++ ScrapeSenatorFDs.py
@@ -1,74 +1,39 @@
 # A script to track recent stock trades by Congress members
 import csv
 import time
+import sqlite3
+from sqlite3 import Error
 
 import pandas as pd
 from selenium import webdriver
 from selenium.webdriver.common.action_chains import ActionChains
 
 
-def scrape_senate_efds():
-    # Open the webpage
+# Scrape the report data from the senate EFD website & store in CSV
+def scrape_headers(driver, connection):
+    # Open the web page
     url = 'https://efdsearch.senate.gov/search/'
-    DRIVER_PATH = 'C:\\bin\chromedriver.exe'
-    driver = webdriver.Chrome(executable_path=DRIVER_PATH)
     driver.get(url)
-    # Agree not to use the data for anything illegal
-    agree_checkbox = driver.find_element_by_id('agree_statement')
-    agree_checkbox.click()
-    # Limit the selections to active senators, then click search. The Periodic Transactions checkbox would filter out amendments and cannot be used.
-    senator_checkbox = driver.find_element_by_class_name('form-check-input.senator_filer')
-    senator_checkbox.click()
-    search_button = driver.find_element_by_class_name('btn.btn-primary')
-    search_button.click()
 
-    # Show 100 results, search for Periodic Transaction Reports, and sort descending by date filed
-    results_dropdown = driver.find_element_by_class_name('form-control.ml-1.mr-1')
-    results_dropdown.send_keys('100')
-    time.sleep(1)
-    filter_search = driver.find_element_by_class_name('form-control.table__search.ml-2')
-    filter_search.send_keys('Periodic Transaction Report')
-    time.sleep(1)
-    date_header = driver.find_element_by_xpath('//*[contains(@aria-label,\'Date\')]')
-    date_header.click()
-    time.sleep(1)
-    date_header.click()
-    time.sleep(1)
+    # Search for the periodic transaction reports of active senators
+    driver.find_element_by_id('agree_statement').click()    # Agree not to use the data for anything illegal
+    driver.find_element_by_class_name('form-check-input.senator_filer').click()     # Check the senator checkbox
+    driver.find_element_by_class_name('btn.btn-primary').click()    # Click search
+    driver.find_element_by_class_name('form-control.ml-1.mr-1').send_keys('100')    # Show 100 results
+    time.sleep(1)   # Wait a second
+    driver.find_element_by_class_name('form-control.table__search.ml-2').send_keys('Periodic Transaction Report')   # Search for PTRs
+    time.sleep(1)   # Wait a second
+    driver.find_element_by_xpath('//*[contains(@aria-label,\'Date\')]').click()     # Sort by ascending date
+    time.sleep(1)   # Wait a second
 
-    # # TO BE ADDED - some sort of way to only scrape new reports instead of doing a full read each time
-    # if path.exists('header.csv') and path.exists('transactions.csv'):
-    #     report_headers = pd.read_csv('header.csv')
-    #     report_transactions = [list(row) for row in pd.read_csv('transactions.csv').values]
-    # else:
-
-    # If there are no existing files, initialize empty lists to store data
-    report_headers, report_transactions = ([] for i in range(2))
+    # Initialize empty lists to store data
+    report_headers, report_links, report_transactions = ([] for i in range(3))
 
     # Loop through each page
     while True:
-        # Pull header info from rows of table - relies on table being 5 columns in the correct order
+        # Pull header info & report links - relies on table being 5 columns in the correct order
         report_headers.extend([cell.text for cell in driver.find_elements_by_xpath('.//td')])
-
-        # Store list of links to be opened
-        report_links = driver.find_elements_by_xpath('//a[contains(@href,\'search/view\')]')
-
-        # Ensure the first report is within view
-        actions = ActionChains(driver)
-        actions.move_to_element(report_links[0]).perform()
-
-        # Store the transactions of each report
-        for link in report_links:
-            # Open report in new tab & switch to it
-            link.click()
-            driver.switch_to.window(driver.window_handles[1])
-
-            # Store all transactions as a single list - relies on table being
-            report_transactions.append(
-                [cell.text.replace('\n', ' ') for cell in driver.find_elements_by_xpath('.//td')])
-
-            # Close the report tab & switch back to search results
-            driver.close()
-            driver.switch_to.window(driver.window_handles[0])
+        report_links.extend([link.get_attribute('href') for link in driver.find_elements_by_xpath('//a[contains(@href,\'search/view\')]')])
 
         # If the page just scraped was the last page, the scraping process is done
         if driver.find_elements_by_class_name('paginate_button')[-2].get_attribute('class') == 'paginate_button current':
@@ -80,46 +45,87 @@ def scrape_senate_efds():
             driver.find_element_by_class_name('paginate_button.next').click()
             time.sleep(2)
 
-        # Split header data into 4 'parallel' lists
-        first_name, last_name, report_title, date_filed, senate_trades = ([] for i in range(5))
-        first_name.extend(report_headers[0::5])
-        last_name.extend(report_headers[1::5])
-        report_title.extend(report_headers[3::5])
-        date_filed.extend(report_headers[4::5])
+    # Split header data into 'parallel' lists
+    first_name, last_name, report_title, date_filed = ([] for i in range(4))
+    first_name.extend(report_headers[0::5])
+    last_name.extend(report_headers[1::5])
+    report_title.extend(report_headers[3::5])
+    date_filed.extend(report_headers[4::5])
 
-        # Create header data frame
-        header_list = list(zip(first_name, last_name, report_title, date_filed))
-        header_df = pd.DataFrame(header_list, columns=['first_name', 'last_name', 'report_title', 'date_filed'])
+    # Create header data frame
+    header_list = list(zip(first_name, last_name, report_title, date_filed, report_links))
+    header = pd.DataFrame(header_list, columns=['first_name', 'last_name', 'report_title', 'date_filed', 'report_link'])
 
-        # Export data to CSVs for logging
-        header_df.to_csv('header.csv')
-        with open('transactions.csv', 'w', newline='') as f:
-            writer = csv.writer(f)
-            writer.writerows(report_transactions)
+    # Remove PDF reports
+    header = header[~header.report_link.str.contains('paper')].reset_index(drop=True)
+
+    # Remove reports that have more recent amendments
+    header = ignore_amended(header).reset_index(drop=True)
+
+    # Export data
+    header.to_sql(name='header', con=connection, if_exists='replace', index_label='report_id')
+    header.to_csv('header.csv')
 
 
-# Combine header data & transaction data
-def merge_data():
-    # Read header CSV file into a data frame
-    header = pd.read_csv('header.csv', index_col=0)
+# Scrape transactions  -- not revised to use database yet
+def scrape_transactions(browser, connect):
+    # Open the web page & agree not to use the data for anything illegal
+    url = 'https://efdsearch.senate.gov/search/'
+    browser.get(url)
+    browser.find_element_by_id('agree_statement').click()
 
-    # Read transactions CSV file as a list of lists
-    with open('transactions.csv', 'r') as read_obj:
-        csv_reader = csv.reader(read_obj)
-        transactions = list(csv_reader)
+    # Pull the header table
+    hdr = pd.read_sql_query('SELECT * FROM header', con=connect, index_col='report_id')
 
-    # Determine which reports have been amended later and should be ignored
-    amendments = header[header['report_title'].str.contains('Amendment')].drop(columns=['date_filed'])
+    all_transactions = []
+
+    # Store the transactions of each report
+    for index, link in hdr.iterrows():
+        print('link = ', link['report_link'])
+        # Open report in new tab & switch to it
+        browser.get(link['report_link'])
+        time.sleep(2)
+
+        # Store all transactions as a single list
+        report_transactions = list(split_list(list(cell.text for cell in browser.find_elements_by_xpath('.//td')), 9))
+
+        # Add relevant report id for linking to header table
+        for trn in report_transactions:
+            trn.insert(0, index)
+            all_transactions.append(trn)
+
+    # Data frame for transactions
+    transactions = pd.DataFrame(all_transactions, columns=['report_id', 'transaction_id', 'transaction_date', 'owner', 'security',
+                                                           'company', 'security_type', 'transaction_type', 'amount_range', 'comment'])
+
+    # Export data
+    transactions.to_sql(name='transactions', con=connect, if_exists='replace', index_label='mstr_trn_id')
+    transactions.to_csv('transactions.csv')
+
+
+# Give an overview of the database
+def view_database(con):
+    all_data = pd.read_sql_query('SELECT * FROM header, transactions WHERE header.report_id = transactions.report_id', con=con, index_col='mstr_trn_id')
+    print(all_data)
+    all_data.to_csv('SenateEFDs.csv')
+
+
+# Ignore reports that have been amended. Based on report title. Avoids unnecessary transaction scraping.
+def ignore_amended(df):
+    # Store list of report titles that contain 'Amendment'
+    amendments = df[df['report_title'].str.contains('Amendment')].drop(columns=['date_filed'])
     ignore = []
+
+    # Loop through the list of amended reports
     for ind, amd in amendments.iterrows():
-        # Store the title of the report without any amendments
+        # Store the root title of the report
         title = amd.report_title[0:len('Periodic Transaction Report for 00/00/0000')]
 
         # Store the version (number after amendment)
         try:
             version = int(amd.report_title[-2:-1])
         except:
-            version = 1  # If a version if not listed, assume it is the first and only amendment
+            version = 1  # If a version is not listed, assume it is the first and only amendment
 
         # Store the information for the reports which should be ignored
         for v in range(version):
@@ -133,27 +139,45 @@ def merge_data():
         writer = csv.writer(fl)
         writer.writerows(ignore)
 
-    # Create master data set by looping through each set of transactions
-    senate_trades = []
-    for rep in range(len(transactions)):
-        # Skip reports on the ignore list
-        if not [header['first_name'][rep], header['last_name'][rep], header['report_title'][rep]] in ignore:
-            # For each existing transaction, save the header & transaction information
-            for trns in range(int(len(transactions[rep]) / 9)):
-                senate_trades.append([header['first_name'][rep], header['last_name'][rep], header['report_title'][rep],
-                                      header['date_filed'][rep], transactions[rep][9 * trns],
-                                      transactions[rep][9 * trns + 1], transactions[rep][9 * trns + 3],
-                                      transactions[rep][9 * trns + 4], transactions[rep][9 * trns + 5],
-                                      transactions[rep][9 * trns + 6], transactions[rep][9 * trns + 7],
-                                      transactions[rep][9 * trns + 8]])
-    senate_efds = pd.DataFrame(senate_trades,
-                               columns=['first_name', 'last_name', 'report_title', 'date_filed', 'transaction_id',
-                                        'transaction_date', 'security', 'company', 'security_type', 'transaction_type',
-                                        'amount_range', 'comment'])
-    # Write master data set to CSV
-    senate_efds.to_csv('Senate EFDs.csv')
+    # Remove rows matching the ignore list
+    for row in range(len(df)):
+        unique = [df['first_name'][row], df['last_name'][row], df['report_title'][row]]
+        if unique in ignore:
+            df = df.drop([row])
+
+    # Return the data frame
+    return df
 
 
-# Call functions as necessary
-# scrape_senate_efds()      # Comment out to avoid lengthy scraping process when testing data manipulation
-merge_data()
+# Split a long list 'list' into smaller lists of length 'n'
+def split_list(lst, n):
+    for i in range(0, len(lst), n):
+        yield lst[i:i + n]
+
+
+# Chrome driver path
+driver_path = 'C:\\bin\\chromedriver.exe'
+# Database path
+db_path = 'C:\\Users\\RyanDodds\\Documents\\GitHub\\Senator_Stock_Trading\\SenateEFDs.db'
+
+# Turn function calls on / off
+scrp_headers = False
+scrp_transactions = False
+view = True
+
+# Scrape new reports / transactions
+if scrp_headers:
+    hd_driver = webdriver.Chrome(executable_path=driver_path)
+    hd_conn = sqlite3.connect(db_path)
+    scrape_headers(hd_driver, hd_conn)
+
+# Scrape new transactions
+if scrp_transactions:
+    trn_driver = webdriver.Chrome(executable_path=driver_path)
+    trn_conn = sqlite3.connect(db_path)
+    scrape_transactions(trn_driver, trn_conn)
+
+# Let's actually use that data we worked so hard to get
+if view:
+    view_conn = sqlite3.connect(db_path)
+    view_database(view_conn)
